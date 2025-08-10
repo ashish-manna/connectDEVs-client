63b033674783464672e85e878c9ba56cc6aff3b9
ashish-manna <m.ashish1920@gmail.com>
2025-08-10 17:18:30 +0530

    basic redux setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Layout from './pages/Layout'
+import { Provider } from 'react-redux'
+import appStore from './redux/appStore'
+import Feed from './pages/Feed'
 
 const App = () => {
   return (
-    <BrowserRouter basename='/'>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='/login' element={<Login />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={appStore}>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Feed />} />
+            <Route path='/login' element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,39 @@
+import { useSelector } from "react-redux"
+
+
 const Navbar = () => {
+
+    const user = useSelector((store) => store.user);
+
     return (
         <div className="navbar bg-base-300 shadow-sm">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">ConnectDEVs</a>
             </div>
             <div className="flex gap-2">
-                <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-                <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCpY5LtQ47cqncKMYWucFP41NtJvXU06-tnQ&s" />
+                {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
+                {user && <>
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src={`${import.meta.env.VITE_DUMMY_IMG_URL}`} />
+                            </div>
                         </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-300 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-300 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
-                </div>
+                </>}
             </div>
         </div>
     )
